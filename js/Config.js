bd2c312f2c877bc331200d0b9ac2d597459480bb
kurokuro15 <kurokuro15000@gmail.com>
2022-03-20 17:28:10 -0400

    adding addresses collection

--- js/Config.js
+++ js/Config.js
@@ -8,5 +8,18 @@ export default {
     },
     { type: 'Sobre', size: 'small', minWeight: 0, maxWeight: 500 }
   ],
-  dimensions: { L: ['length', 'largo'], W: ['width', 'ancho'], H: ['high', 'alto'] }
+  dimensions: { L: ['length', 'largo'], W: ['width', 'ancho'], H: ['high', 'alto'] },
+  addresses: [
+    { name: 'A', x: 150, y: 300 },
+    { name: 'B', x: 300, y: 120 },
+    { name: 'C', x: 100, y: 80 },
+    { name: 'D', x: 20, y: 90 },
+    { name: 'E', x: 1500, y: 370 },
+    { name: 'F', x: 200, y: 800 },
+    { name: 'G', x: 120, y: 1500 },
+    { name: 'H', x: 1340, y: 1350 },
+    { name: 'I', x: 1270, y: 1790 },
+    { name: 'J', x: 777, y: 433 },
+    { name: 'K', x: 333, y: 666 }
+  ]
 }
