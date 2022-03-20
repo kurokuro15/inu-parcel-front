1b926b25b442cefab1991ff90cc855b1f5884f91
kurokuro15 <kurokuro15000@gmail.com>
2022-03-20 18:46:44 -0400

    add shipping addresses

--- js/Config.js
+++ js/Config.js
@@ -10,16 +10,16 @@ export default {
   ],
   dimensions: { L: ['length', 'largo'], W: ['width', 'ancho'], H: ['high', 'alto'] },
   addresses: [
-    { name: 'A', x: 150, y: 300 },
-    { name: 'B', x: 300, y: 120 },
-    { name: 'C', x: 100, y: 80 },
-    { name: 'D', x: 20, y: 90 },
-    { name: 'E', x: 1500, y: 370 },
-    { name: 'F', x: 200, y: 800 },
-    { name: 'G', x: 120, y: 1500 },
-    { name: 'H', x: 1340, y: 1350 },
-    { name: 'I', x: 1270, y: 1790 },
-    { name: 'J', x: 777, y: 433 },
-    { name: 'K', x: 333, y: 666 }
+    { id: 1, name: 'A', x: 150, y: 300 },
+    { id: 2, name: 'B', x: 300, y: 120 },
+    { id: 3, name: 'C', x: 100, y: 80 },
+    { id: 4, name: 'D', x: 20, y: 90 },
+    { id: 5, name: 'E', x: 1500, y: 370 },
+    { id: 6, name: 'F', x: 200, y: 800 },
+    { id: 7, name: 'G', x: 120, y: 1500 },
+    { id: 8, name: 'H', x: 1340, y: 1350 },
+    { id: 9, name: 'I', x: 1270, y: 1790 },
+    { id: 10, name: 'J', x: 777, y: 433 },
+    { id: 11, name: 'K', x: 333, y: 666 }
   ]
 }
