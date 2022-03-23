2760f9d6f11208a24e60beed7b3a7287cb76d58d
kurokuro15 <kurokuro15000@gmail.com>
2022-03-23 09:08:53 -0400

    update directory path

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
-import { App } from './App.js'
+import { App } from './Classes/App.js'
+import Parcel from './Classes/Parcel.js'
 
 const app = new App()
 app.init()
+
+const prop = { value: 100, dimensions: { length: 2, width: 3, high: 5 }, weight: 0.1, type: 'paquete', senderAddress: 1, receivingAddress: 2 }
+const parcel = new Parcel(prop)
+console.log(parcel.getParcel())
