06ed09be151aadbe5e70f43bfcb7033bdb07e0eb
kurokuro15 <kurokuro15000@gmail.com>
2022-03-24 22:59:37 -0400

    Create selecter and createElement and parcelData and validationWhatSend functions and parcelObj

--- js/GlobalSelectors.js
+++ js/GlobalSelectors.js
@@ -1,7 +1,50 @@
+
 // Acá estarán ubicados selectores para usar globalmente y funciones que requieren del dom
 
 // Selectors
-export const container = document.querySelector('div.container')
-
+export const container = globalThis.document.querySelector('div.container')
+export const selecter = selector => globalThis.document.querySelector(selector)
 // Functions
-export const createElement = (element) => globalThis.document.createElement(element)
+export const createElement = element => globalThis.document.createElement(element)
+
+export const parcelObj = {
+  value: 0,
+  length: 0,
+  width: 0,
+  high: 0,
+  weight: 0,
+  type: '',
+  senderAddress: 0,
+  receivingAddress: 0
+}
+
+export function parcelData (e) {
+  parcelObj[e.target.id] = e.target.value
+  console.log(parcelObj)
+}
+
+export function validationWhatSend (e, callback) {
+  const montReg = /\d+\.?\d*/
+  const { value, length, width, high, weight, type } =
+    parcelObj
+  if (
+    !value ||
+    !length ||
+    !width ||
+    !high ||
+    !weight ||
+    !type
+  ) {
+    return 'Todos los campos son requeridos.'
+  }
+  if (isNaN(value) || !montReg.test(value)) {
+    return 'Ingrese un monto válido'
+  }
+  if ((isNaN(length) || isNaN(width) || isNaN(high)) || (!montReg.test(length) || !montReg.test(width) || !montReg.test(high))) {
+    return 'Ingrese una medida válida'
+  }
+  if (isNaN(weight) || !montReg.test(weight)) {
+    return 'Ingrese un peso válido'
+  }
+  callback()
+}
