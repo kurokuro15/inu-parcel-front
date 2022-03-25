98ca0d51d76debc948e7545905dbf76492128f29
kurokuro15 <kurokuro15000@gmail.com>
2022-03-24 22:59:49 -0400

    remove parcel test :D

--- js/main.js
+++ js/main.js
@@ -1,9 +1,4 @@
 import { App } from './Classes/App.js'
-import Parcel from './Classes/Parcel.js'
 
 const app = new App()
 app.init()
-
-const prop = { value: 100, dimensions: { length: 2, width: 3, high: 5 }, weight: 0.1, type: 'paquete', senderAddress: 1, receivingAddress: 2 }
-const parcel = new Parcel(prop)
-console.log(parcel.getParcel())
