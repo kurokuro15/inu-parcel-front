1b9f05ed5119a8105e3104836455ad8be3cd8ccf
Reynaldo González <kurokuro15000@gmail.com>
2022-04-03 19:32:36 -0400

    add comment

--- js/Classes/App.js
+++ js/Classes/App.js
@@ -6,6 +6,7 @@ export class App {
   }
 
   init () {
+    // acá puedo indicar si está la sesión iniciada o en caso contrario no se ha registrado/iniciado sesión.
     this.ui.header()
     this.ui.main()
     console.log('iniciando app...')
