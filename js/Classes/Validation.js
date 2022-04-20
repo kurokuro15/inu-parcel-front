12a082fb0da3de7f78c33d71d562beb24085253e
kurokuro15 <kurokuro15000@gmail.com>
2022-04-20 10:08:57 -0400

    refactor: reposition of sentences and methods of Validator

--- js/Classes/Validation.js
+++ js/Classes/Validation.js
@@ -1,4 +1,4 @@
-import { formToJSON } from '../GlobalSelectors.js'
+import { formToJSON, selecter } from '../GlobalSelectors.js'
 import Ui from './Ui.js'
 export default class Validator {
   constructor () {
@@ -11,9 +11,8 @@ export default class Validator {
    * @param {function} callback Llamada de vuelta a una función que se ejecutará si la validación resulta.
    */
   static validateSignin (e, callback) {
-    e.preventDefault()
     Validator.ui = new Ui()
-    Validator.data = formToJSON(e.target)
+    Validator.data = formToJSON(e)
     const today = new Date()
     const {
       birthday,
@@ -32,9 +31,13 @@ export default class Validator {
       state,
       street,
       username,
-      zipcode
+      zipcode,
+      questionOne,
+      answerOne,
+      questionTwo,
+      answerTwo
     } = Validator.data
-    // console.log(Validator.data)
+    console.log(Validator.data)
 
     // validamos que no estén vacíos los requeridos
     if (
@@ -53,41 +56,55 @@ export default class Validator {
         sex &&
         state &&
         street &&
-        username
+        username &&
+        questionOne &&
+        answerOne &&
+        questionTwo &&
+        answerTwo
       )
     ) {
-      return this.ui.printAlert('error', 'Los campos son obligatorios.')
+      this.ui.printAlert('error', 'Los campos marcados son obligatorios.')
+      for (const key in Validator.data) {
+        if (Object.hasOwnProperty.call(Validator.data, key)) {
+          const element = Validator.data[key]
+          if (!element) {
+            if (key === 'reference') {
+              continue
+            }
+            this.ui.alertInput('error', selecter(`#${key}`))
+          }
+        }
+      }
+      return false
     }
 
     // Validamos Username
     const maxUsernameLength = 12
     if (Validator._validateField(username, maxUsernameLength)) {
-      return this.ui.alertInput('error', e.target.querySelector('#username'))
+      return this.ui.alertInput('error', e.querySelector('#username'))
     }
 
     // Validamos Nombres y apellidos
     const maxNameLength = 26
     const refName = Validator._returnTheReference(name)
     if (Validator._havMaxLength(name, maxNameLength)) {
-      return (() => {
-        this.ui.printAlert(
-          'error',
-          `Máximo ${maxNameLength} carácteres en el campo ${refName}`
-        )
-        this.ui.alertInput('error', e.target.querySelector('#name'))
-      })()
+      this.ui.printAlert(
+        'error',
+        `Máximo ${maxNameLength} carácteres en el campo ${refName}`
+      )
+      this.ui.alertInput('error', e.querySelector('#name'))
+      return false
     }
 
     const maxLastnameLength = 26
     const refLastname = Validator._returnTheReference(lastname)
     if (Validator._havMaxLength(lastname, maxLastnameLength)) {
-      return (() => {
-        this.ui.printAlert(
-          'error',
-          `Máximo ${maxLastnameLength} carácteres en el campo ${refLastname}`
-        )
-        this.ui.alertInput('error', e.target.querySelector('#lastname'))
-      })()
+      this.ui.printAlert(
+        'error',
+        `Máximo ${maxLastnameLength} carácteres en el campo ${refLastname}`
+      )
+      this.ui.alertInput('error', e.querySelector('#lastname'))
+      return false
     }
 
     // revisar que no hayan espacios en algunos fields
@@ -97,72 +114,89 @@ export default class Validator {
       Validator._havSpace(phone) ||
       Validator._havSpace(zipcode)
     ) {
-      return this.ui.printAlert(
+      this.ui.printAlert(
         'error',
         'no puede haber espacios en el campo usuario, contraseña, código postal, teléfono '
       )
+      return false
     }
 
     // Validamos que la contraseña tenga más de 8 carácteres, minúsculas, mayúsculas y símbolos.
     const passRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/
     if (!passRegex.test(password)) {
-      return (() => {
-        this.ui.printAlert('error', 'Ingrese una contraseña válida')
-        this.ui.alertInput('error', e.target.querySelector('#password'))
-      })()
+      this.ui.printAlert('error', 'Ingrese una contraseña válida')
+      this.ui.alertInput('error', e.querySelector('#password'))
+      return false
     }
 
     // Validamos que la fecha de nacimiento sea válida (no sea mayor al día de hoy)
     if (new Date(birthday) > today) {
-      return (() => {
-        this.ui.printAlert('error', 'Fecha de nacimiento inválida.')
-        this.ui.alertInput('error', e.target.querySelector('#birthday'))
-      })()
+      this.ui.printAlert('error', 'Fecha de nacimiento inválida.')
+      this.ui.alertInput('error', e.querySelector('#birthday'))
+      return false
     }
 
     // Validamos el correo electrónico
     const emailRegex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,})+$/
     if (!emailRegex.test(email)) {
-      return (() => {
-        this.ui.printAlert('error', 'Correo electrónico inválido')
-        this.ui.alertInput('error', e.target.querySelector('#email'))
-      })()
+      this.ui.printAlert('error', 'Correo electrónico inválido')
+      this.ui.alertInput('error', e.querySelector('#email'))
+      return false
     }
 
     // Validamos el teléfono celular
     const phoneRegex = /^\x2b(\d{0,3})\(?(\d{3})\)?[-]?(\d{3})[-]?(\d{4})$/
     if (!phoneRegex.test(phone)) {
-      return (() => {
-        this.ui.printAlert('error', 'Número de teléfono erróneo')
-        this.ui.alertInput('error', e.target.querySelector('#phone'))
-      })()
+      this.ui.printAlert('error', 'Número de teléfono erróneo')
+      this.ui.alertInput('error', e.querySelector('#phone'))
+      return false
     }
     const dniRegex = /^[VvEeJjGg]-\d{0,3}.\d{0,3}.\d{0,3}-?\d{0,1}/
     if (!dniRegex.test(dni)) {
-      return (() => {
-        this.ui.printAlert('error', 'Número de Cédula erróneo')
-        this.ui.alertInput('error', e.target.querySelector('#dni'))
-      })()
+      this.ui.printAlert('error', 'Número de Cédula erróneo')
+      this.ui.alertInput('error', e.querySelector('#dni'))
+      return false
     }
 
-    // Validamos el campo de referencia
+    // Validamos el campo de referencia (este puede estar vacío)
     const maxReferenceLength = 120
     const refReference = Validator._returnTheReference(reference)
     if (Validator._havMaxLength(reference, maxReferenceLength)) {
-      return (() => {
-        this.ui.printAlert(
-          'error',
-          `Máximo ${maxReferenceLength} carácteres en el campo ${refReference}`
-        )
-        this.ui.alertInput('error', e.target.querySelector('#reference'))
-      })()
+      this.ui.printAlert(
+        'error',
+        `Máximo ${maxReferenceLength} carácteres en el campo ${refReference}`
+      )
+      this.ui.alertInput('error', e.querySelector('#reference'))
+      return false
     }
 
+    // Validamos las respuestas de seguridad (no pueden estar vacíos o contener cosas raras)
+    const maxAnswerLength = 24
+    const aOneRef = Validator._returnTheReference(answerOne)
+    if (Validator._havMaxLength(answerOne, maxAnswerLength)) {
+      this.ui.printAlert('error', `Máximo ${maxAnswerLength} carácteres en el campo ${aOneRef}`)
+      this.ui.alertInput('error', e.querySelector('#reference'))
+      return false
+    }
+
+    const aTwoRef = Validator._returnTheReference(answerTwo)
+    if (Validator._havMaxLength(answerTwo, maxAnswerLength)) {
+      this.ui.printAlert('error', `Máximo ${maxAnswerLength} carácteres en el campo ${aTwoRef}`)
+      this.ui.alertInput('error', e.querySelector('#reference'))
+      return false
+    }
+
+    if (questionOne === questionTwo) {
+      this.ui.printAlert('error', 'Seleccione dos preguntas de seguridad diferentes')
+      this.ui.alertInput('error', selecter('#questionOne'))
+      this.ui.alertInput('error', selecter('#questionTwo'))
+      return false
+    }
     // Si todo pasa entonces:
-    // Guardamos en localstorage el objeto validado.
-    globalThis.localStorage.setItem('signinForm', JSON.stringify(Validator.data))
+    // enviamos la petición we :v
     // llamamos a la función que imprimirá la siguiente vista
-    callback()
+    // na solo mandamos un true :v
+    return true
   }
 
   /**
